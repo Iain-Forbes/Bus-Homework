4f31227fef44728e750a0315e5853871efc305f5
Iain Forbes <i.forbes77@gmail.com>
2020-11-10 23:49:27 +0000

    3nd commit, added add/clear queue

--- src/bus.py
+++ src/bus.py
@@ -18,16 +18,24 @@ class Bus():
     def drop_off(self, passengers_2):
         self.passengers.remove(passengers_2)
         
-
     def empty(self):
         self.passengers.clear()
 
-    def can_pick_up_passenger_from_bus_stop(self, passenger, busstop):
-        pass  
+    def pick_up_from_stop(self, passengers_1):
+        self.pick_up()
+        self.pick_up()
 
-        
-        
 
     
+        
+       
 
-  
+    #     @unittest.skip("Delete this line to run the test")
+    # def test_can_pick_up_passenger_from_bus_stop(self):
+    #     person_1 = Person("Guido van Rossum", 64)
+    #     person_2 = Person("Carol Willing", 50)
+    #     bus_stop = BusStop("Waverly Station")
+    #     bus_stop.add_to_queue(person_1)
+    #     bus_stop.add_to_queue(person_2)
+        # self.bus.pick_up_from_stop(bus_stop)
+        # self.assertEqual(2, self.bus.passenger_count())
